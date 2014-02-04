2a68b59da084a76dd493ea2f95aa0a276a0656a0
MeganCrane1 <megan.crane1@marist.edu>
2014-02-04 15:52:34 -0500

    Megan Crane 2/4/14 Lab 3
    SQL queries from CAP2 database

---
+++ Lab3.sql
@@ -0,0 +1,57 @@
+﻿-- Q1
+
+select city, name
+from agents
+where name = 'Smith'
+
+-- Q2
+
+select pid, name, quantity
+from products
+where priceusd > 1.25
+
+-- Q3
+
+select ordno, aid
+from orders
+
+-- Q4
+
+select name, city
+from customers
+where city = 'Dallas'
+
+-- Q5
+
+select name
+from agents
+where city != 'New York' 
+AND city != 'Newark'
+
+-- Q6
+
+select *
+from products
+where city != 'New York'
+AND city != 'Newark'
+AND priceusd >= 1.00
+
+-- Q7
+
+select *
+from orders
+where mon != 'jan'
+AND mon != 'mar'
+
+-- Q8
+
+select *
+from orders
+where mon = 'feb'
+AND dollars < 100.00
+
+-- Q9
+
+select *
+from orders
+where cid = 'c001'
